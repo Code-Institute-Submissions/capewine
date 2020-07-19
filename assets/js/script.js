1edf130be8eabed7d4d4a28f28d04fa24e0fe836
lucyrush <lucy@codeinstitute.net>
2020-07-19 15:40:08 +0000

    API key, map HTML and map initialising added.Problem:Map not loading

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// function initMap() {
+//     var map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 3,
+//         center: {
+//             lat: 46.619261,
+//             lng: -33.134766
+//         }
+//     });
+// }
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 53.42620769757938, lng: -7.823638916015626 },
+        zoom: 6.8
+    });
+}
