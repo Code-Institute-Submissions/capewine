917810352ed430696d0aa2474a32c1d723d5d2ae
lucyrush <lucy@codeinstitute.net>
2020-07-22 20:35:06 +0000

    Initialise map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-// function initMap() {
-//     var map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 3,
-//         center: {
-//             lat: 46.619261,
-//             lng: -33.134766
-//         }
-//     });
-// }
+var map, places, infoWindow, option;
+var markers = [];
+var autocomplete;
+var countryRestrict = { 'country': 'za' };
+var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
+var hostnameRegexp = new RegExp('^https?://.+?/');
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 33.9249, lng: 18.4241 },
+        center: { lat: -34.270836, lng: 18.459778 },
         zoom: 6.8
     });
-}
+
+    infoWindow = new google.maps.InfoWindow({
+        content: document.getElementById('info-content')
+    });
